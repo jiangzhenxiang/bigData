d1aaf92cf67a8b1e74a1fb369757fe9f100eb412
姜振祥 <jiangzx@jiedaibao.com>
2020-09-10 21:45:26 +0800

    async render

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import RealList from "./page/realList";
 import VirtualList from './page/virtualList';
-
+import AsyncRenderList from "./page/asyncRender";
 function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-            </header>
             <div>
-                <VirtualList></VirtualList>
+                {/*<VirtualList></VirtualList>*/}
                 {/*<RealList></RealList>*/}
+                {/*<AsyncRenderList></AsyncRenderList>*/}
             </div>
         </div>
     );
---
+++ react-app/src/page/asyncRender.js
@@ -0,0 +1,53 @@
+import React, {useEffect, useState} from 'react';
+import '../App.css';
+
+
+function AsyncRenderList() {
+    const [list, setList] = useState([]);
+    const [asyncList, setAsyncList] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:3000')
+            .then(response => response.json())
+            .then(body => {
+                setList(body);
+            })
+    }, [])
+
+    useEffect(() => {
+        let index = 0;
+        let total = list.length;
+        function load() {
+            index += 50;
+            if (index < total) {
+                setTimeout(() => {
+                    let temp = list.slice(index, index + 50);
+                    setAsyncList((asyncList) => [...asyncList, ...temp])
+                    load();
+                },0)
+            }
+        }
+        load();
+    }, [list])
+
+    return (
+        <div className="App">
+            <div>
+                <div>异步渲染列表</div>
+                <div>
+                    {asyncList.map((item, index) => {
+                        const {name, text, title} = item;
+                        return (
+                            <div className="item" key={name}>
+                                <span>{title}-{name}</span>
+                                <span>:{text}</span>
+                            </div>
+                        );
+                    })}
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default AsyncRenderList;
